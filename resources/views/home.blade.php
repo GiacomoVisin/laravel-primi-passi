8a921588cfc210f0155a06b3e8332d6cd5163719
GiacomoVisin <killerboss1150@gmail.com>
2026-09-22 23:06:21 +0200

    creating home route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,48 @@
-<?php 
+<!DOCTYPE html>
+<html lang="it">
 
-echo "hello world"
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
 
-?>
+
+    <style>
+        h1 {
+            text-align: center;
+        }
+
+        ul {
+            display: flex;
+            gap: 3rem;
+            list-style-type: none;
+            color: black;
+            border: 1px solid black;
+            padding: 0.5rem;
+            width: 20rem;
+            justify-content: center;
+            position: relative;
+            top: -50px;
+            left: 20px;
+
+            a {
+                color: black;
+            }
+        }
+    </style>
+
+</head>
+
+<body>
+    <h1> Home </h1>
+
+    <nav>
+        <ul>
+            <li> <a href="{{ route('home') }}">HOME</a> </li>
+            <li> <a href="{{ route('chi-siamo') }}">CHI-SIAMO</a> </li>
+            <li> <a href="{{ route('contatti') }}">CONTATTI</a> </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/chi-siamo', function () {
+    return view('chi-siamo');
+})->name('chi-siamo');
+
+Route::get('/contatti', function () {
+    return view('contatti');
+})->name('contatti');
